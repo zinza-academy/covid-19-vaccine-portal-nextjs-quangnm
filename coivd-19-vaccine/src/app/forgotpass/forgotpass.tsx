10200acc4b8fe5e7fa80b9f69ecaba231b3db41e
quangg2002 <ngominhquang12a2nl@gmail.com>
2024-07-31 10:52:00 +0700

    call-api-forgotpass

--- coivd-19-vaccine/src/app/forgotpass/forgotpass.tsx
+++ coivd-19-vaccine/src/app/forgotpass/forgotpass.tsx
@@ -1,111 +1,160 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import bgforget from '@/asset/image/bgforget.jpg';
-import { Grid, Button, Box, Input, Typography } from '@mui/material';
+import { Grid, Button, Box, TextField, Typography, Alert } from '@mui/material';
+import * as yup from 'yup';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useAppSelector, useAppDispatch } from '../../redux/hooks';
+import { forgotPasswordAsync } from '@/features/user/forgotpassSlice';
+import { indigo } from '@mui/material/colors';
+import { LoadingButton } from '@mui/lab';
+
+const defaultValues = {
+    email: ''
+};
+
+const schema = yup.object({
+    email: yup.string().required().email()
+});
 
 const Forgotpass = () => {
-  const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isFormValid, setIsFormValid] = useState(false);
+    const router = useRouter();
+    const loadingStatus = useAppSelector((state) => state.forgotPassword.loading);
+    const message = useAppSelector((state) => state.forgotPassword.message);
+    const dispatch = useAppDispatch();
 
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const {
+        control,
+        handleSubmit,
+        formState: { isValid }
+    } = useForm({
+        defaultValues,
+        mode: 'onChange',
+        resolver: yupResolver(schema)
+    });
 
-  useEffect(() => {
-    setIsFormValid(emailPattern.test(email));
-  }, [email]);
+    const onSubmit = async (data: {email: string}) => {
+        await dispatch(forgotPasswordAsync(data));
+    };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+    const handleBack = () => {
+        router.push('/');
+    };
 
-    if (!email) {
-      alert('Email is required');
-      return;
-    }
-
-    setIsSubmitting(true);
-
-    setTimeout(() => {
-      setIsSubmitting(false);
-      router.push('');
-    }, 2000);
-  };
-
-  return (
-    <Grid container sx={{ height: '100vh' }}>
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={6}
-        sx={{
-          backgroundImage: `url(${bgforget.src})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundColor: t =>
-            t.palette.mode === 'light'
-              ? t.palette.grey[50]
-              : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
-      <Grid
-        xs={false}
-        sm={8}
-        md={6}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        sx={{ display: 'flex' }}
-      >
-        <Box component="form" onSubmit={handleSubmit} className="w-75">
-          <Grid className="text-center mb-3">
-            <Typography variant="body1" component="span">
-              Để khôi phục mật khẩu, vui lòng đăng nhập đúng email đã dùng để
-              đăng ký
-              <Typography variant="body1" component="span" color="error">
-                {' '}
-                (*)
-              </Typography>
-            </Typography>
-          </Grid>
-          <Input
-            type="email"
-            placeholder="user@gmail.com"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            className="form-control mb-3"
-            required
-          />
-          <Grid
-            item
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            sx={{ display: 'flex' }}
-          >
-            <Button
-              variant="outlined"
-              sx={{ mr: 1 }}
-              onClick={() => router.back()}
-            >
-              Quay lại
-            </Button>
-
-            <Button
-              type="submit"
-              variant="contained"
-              disabled={isSubmitting || !isFormValid}
-            >
-              {isSubmitting ? 'Gửi...' : 'Gửi'}
-            </Button>
-          </Grid>
-        </Box>
-      </Grid>
-    </Grid>
-  );
+    return (
+        <Grid container sx={{ height: '100vh' }}>
+            <Grid
+                item
+                xs={false}
+                sm={6}
+                md={6}
+                sx={{
+                    backgroundImage: `url(${bgforget.src})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                        t.palette.mode === 'light'
+                            ? t.palette.grey[50]
+                            : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                }}
+            />
+            <Grid
+                xs={12}
+                sm={6}
+                md={6}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                display={'flex'}
+                >
+                  
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit(onSubmit)}
+                    justifyContent="center"
+                    alignItems="center"
+                    flexDirection={'column'}
+                    display={'flex'}
+                    >
+                    {message && (
+                        <Alert severity="error" sx={{ mb: 2 }}>
+                            {message}
+                        </Alert>
+                    )}
+                    <Grid textAlign={'center'} width={'80%'}>
+                        <Typography variant="body1" component="span">
+                            Để khôi phục mật khẩu, vui lòng đăng nhập đúng email
+                            đã dùng để đăng ký
+                            <Typography
+                                variant="body1"
+                                component="span"
+                                color="error">
+                                {' '}
+                                (*)
+                            </Typography>
+                        </Typography>
+                    </Grid>
+                    <Controller
+                        name="email"
+                        control={control}
+                        render={({ field, fieldState: { error } }) => (
+                            <TextField
+                                sx={{
+                                    width: '80%',
+                                    marginTop: 1,
+                                }}
+                                {...field}
+                                error={!!error}
+                                helperText={error?.message}
+                            />
+                        )}
+                    />
+                    <Grid
+                        item
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        sx={{ display: 'flex' }}>
+                        <Button
+                            onClick={handleBack}
+                            variant="outlined"
+                            sx={{
+                                margin: '12px  16px 12px 0',
+                                borderColor: indigo[400],
+                                borderRadius: '8px',
+                                borderBottomLeftRadius: '0px',  
+                                '&:hover': {
+                                    borderColor: indigo[500],
+                                    backgroundColor: '#fff'
+                                }
+                            }}>
+                            Quay lại
+                        </Button>
+                        <LoadingButton
+                            loading={loadingStatus}
+                            type="submit"
+                            disabled={!isValid}
+                            sx={{
+                                color: '#fff',
+                                backgroundColor: indigo[600],
+                                margin: '12px 0',
+                                borderRadius: '8px',
+                                borderBottomLeftRadius: '0px',  
+                                '&:hover': {
+                                    backgroundColor: indigo[700]
+                                }
+                            }}>
+                            Gửi
+                        </LoadingButton>
+                    </Grid>
+                </Box>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default Forgotpass;
